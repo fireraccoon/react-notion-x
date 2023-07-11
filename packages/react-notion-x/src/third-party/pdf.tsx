8effda5b4e8670053e5d975d06e3e192a71ca1b7
raccooncc <raccooncc@qq.com>
2023-07-11 13:22:13 +0800

    Add support for automatically adjusting the width of pdf

--- packages/react-notion-x/src/third-party/pdf.tsx
+++ packages/react-notion-x/src/third-party/pdf.tsx
@@ -6,17 +6,36 @@ import { Document, Page, pdfjs } from 'react-pdf'
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`
 
 export const Pdf: React.FC<{ file: string }> = ({ file, ...rest }) => {
+
+  const documentRef = React.useRef(null)
+
   const [numPages, setNumPages] = React.useState(null)
 
+  const [pageWidth, setPageWidth] = React.useState(null)
+
   function onDocumentLoadSuccess({ numPages }) {
+    updatePageWidth()
     setNumPages(numPages)
   }
 
+  function updatePageWidth() {
+    setPageWidth(documentRef.current?.offsetWidth)
+  }
+
+  React.useEffect(() => {
+    window.addEventListener('resize', updatePageWidth)
+    return () => {
+      window.removeEventListener('resize', updatePageWidth)
+    }
+  })
+
   return (
-    <Document file={file} onLoadSuccess={onDocumentLoadSuccess} {...rest}>
-      {Array.from(new Array(numPages), (_, index) => (
-        <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-      ))}
-    </Document>
+    <div style={{ width: '100%' }} ref={documentRef}>
+      <Document file={file} onLoadSuccess={onDocumentLoadSuccess} {...rest}>
+        {Array.from(new Array(numPages), (_, index) => (
+          <Page key={`page_${index + 1}`} pageNumber={index + 1} width={pageWidth} />
+        ))}
+      </Document>
+    </div>
   )
 }
